92721f4b1ad0606568e87b5838467af386a266c4
akhning-uzi <blateyiban@gmail.com>
2025-07-07 17:34:11 +0500

    navbar

--- src/App.jsx
+++ src/App.jsx
@@ -2,10 +2,12 @@ import React from "react";
 import { Route, Routes } from "react-router-dom";
 import Home from "./pages/home";
 import Contact from "./pages/contact";
+import Navbar from "./shared/navbar";
 
 const App = () => {
   return (
     <div>
+      <Navbar />
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
