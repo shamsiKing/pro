316fd063e3d2e2848595dcfea5da4d3c2d1a2763
akhning-uzi <blateyiban@gmail.com>
2025-07-07 17:49:00 +0500

    tuliq buldi

--- src/companents/TrustedSlider/TrustedSlider.jsx
+++ src/companents/TrustedSlider/TrustedSlider.jsx
@@ -15,7 +15,76 @@ const logos = [
       "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc95e_bosch.svg",
     alt: "Bosch",
   },
-  // Add more logos as needed
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc93f_CATL-black-1.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc968_CATL-1.svg",
+    alt: "CATL",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc93a_CATL-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc95d_CATL.svg",
+    alt: "CATL Alt",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc939_Chery-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc969_Chery.svg",
+    alt: "Chery",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc93c_citroen-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc96a_citroen.svg",
+    alt: "Citroen",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc940_denso-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc96b_denso.svg",
+    alt: "Denso",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc941_evergrande-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc96c_evergrande.svg",
+    alt: "Evergrande",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc942_foxconn-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc96d_foxconn.svg",
+    alt: "Foxconn",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc943_gac-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc96e_gac.svg",
+    alt: "GAC",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc944_geely-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc96f_geely.svg",
+    alt: "Geely",
+  },
+  {
+    black:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc945_gotion-black.svg",
+    color:
+      "https://cdn.prod.website-files.com/6606a41edd35aa51454cc91c/6606a41edd35aa51454cc970_gotion.svg",
+    alt: "Gotion",
+  },
 ];
 
 const TrustedSlider = () => {
@@ -29,7 +98,10 @@ const TrustedSlider = () => {
       <div className="relative w-full overflow-hidden">
         <div className="flex animate-slide-infinite gap-16">
           {logos.concat(logos).map((logo, index) => (
-            <div key={index} className="flex-shrink-0 relative w-40 h-20">
+            <div
+              key={index}
+              className="flex-shrink-0 relative w-40 h-20 hover:cursor-pointer"
+            >
               <img
                 src={logo.black}
                 alt={logo.alt}
@@ -43,6 +115,8 @@ const TrustedSlider = () => {
             </div>
           ))}
         </div>
+        <div className="absolute left-0 top-0 h-full w-12 bg-gradient-to-r from-white to-transparent z-10" />
+        <div className="absolute right-0 top-0 h-full w-12 bg-gradient-to-l from-white to-transparent z-10" />
       </div>
     </section>
   );
@@ -50,17 +124,16 @@ const TrustedSlider = () => {
 
 export default TrustedSlider;
 
-/* Tailwind CSS - Add this animation to your global styles or tailwind.config.js:
-
- ,
-  } extend: {
-    animation: {
-      'slide-infinite': 'slide 30s linear infinite',
+/* Tailwind CSS (add to tailwind.config.js)
+extend: {
+  animation: {
+    'slide-infinite': 'slide 40s linear infinite',
+  },
+  keyframes: {
+    slide: {
+      '0%': { transform: 'translateX(0)' },
+      '100%': { transform: 'translateX(-50%)' },
     },
-    keyframes: {
-      slide: {
-        '0%': { transform: 'translateX(0)' },
-        '100%': { transform: 'translateX(-50%)' },
-      },
-    }
+  }
+}
 */
