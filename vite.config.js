b4fac7825bd8fc35e85df5fb5674a4db23ae4673
akhning-uzi <blateyiban@gmail.com>
2025-07-07 17:40:39 +0500

    qushdim yana bitta companent

--- vite.config.js
+++ vite.config.js
@@ -1,13 +1,8 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
-import tailwindcss from '@tailwindcss/vite'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
+import tailwindcss from "@tailwindcss/vite";
 
 // https://vite.dev/config/
 export default defineConfig({
-<<<<<<< HEAD
   plugins: [react(), tailwindcss()],
 });
-=======
-  plugins: [react(),tailwindcss()],
-})
->>>>>>> e95b8b41d96a4a17f83308282b88b9fce32e8832
