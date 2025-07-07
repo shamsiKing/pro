fa0e6d963d324933ee7bccc3fd00a07a9f8fec12
khumora77 <xumora427@gmail.com>
2025-07-07 17:26:33 +0500

    asd

---
+++ src/companents/locations/locations.jsx
@@ -0,0 +1,14 @@
+import React from 'react'
+
+const Locations = () => {
+  return (
+    <div>
+      <div>
+        <h1>Offices</h1>
+        <h1>Worldwide Locations</h1>
+      </div>
+    </div>
+  );
+}
+
+export default Locations
