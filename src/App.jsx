9860283ecd5bbf38c53d2f56958ad503ea81c623
khumora77 <xumora427@gmail.com>
2025-07-07 17:20:39 +0500

    contact1

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,14 @@
 import React from 'react'
 import { Route, Routes } from 'react-router-dom'
 import Home from './pages/home'
+import Contact from './pages/contact'
 
 const App = () => {
   return (
     <div>
       <Routes>
-        <Route path='/' element={<Home/>} />
+        <Route path='/' element={<Home />} />
+        <Route path='/contact' element={<Contact/>} />
       </Routes>
     </div>
   )
